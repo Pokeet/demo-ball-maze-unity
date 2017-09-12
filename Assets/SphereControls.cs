c2eda575fc8c7fdcf2b74836c28d9b2591b67acb
TBaudon <baudon.thomas@gmail.com>
2017-09-12 23:59:32 +0200

    mouvement de la sphere

---
+++ Assets/SphereControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereControls : MonoBehaviour {
+
+    public float inputStrength = 10.0f;
+    public float jumpStrength = 500.0f;
+
+    private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.AddForce(Vector3.forward * inputStrength * verticalInput);
+        body.AddForce(Vector3.right * inputStrength * horizontalInput);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            body.AddForce(Vector3.up * jumpStrength);
+        }
+
+    }
+}
